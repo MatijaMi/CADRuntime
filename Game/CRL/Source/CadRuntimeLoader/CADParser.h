1c734745105629f58b4c71996d5a6de2f15ebc20
MatijaMi <matijami12@gmail.com>
2022-07-15 15:16:42 +0200

    Initial Commit

---
+++ Game/CRL/Source/CadRuntimeLoader/CADParser.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "CADParser.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CADRUNTIMELOADER_API UCADParser : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
